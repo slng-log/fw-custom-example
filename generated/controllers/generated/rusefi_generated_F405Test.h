20d832bbab2df0cbba2ffc8818d907c2fdfb9a4f
GitHub git update Action <action@github.com>
2025-11-14 02:02:04 +0000

    Custom Auto-generated Configs
    [no ci]

--- generated/controllers/generated/rusefi_generated_F405Test.h
+++ generated/controllers/generated/rusefi_generated_F405Test.h
@@ -2123,7 +2123,7 @@
 #define ts_show_wastegate_sensor true
 #define ts_show_wbo_canbus_index true
 #define ts_show_wbo_canbus_set_index true
-#define TS_SIGNATURE "rusEFI main.2025.11.13.F405Test.2520102079"
+#define TS_SIGNATURE "rusEFI main.2025.11.14.F405Test.2520102079"
 #define TS_SIMULATE_CAN '>'
 #define TS_SIMULATE_CAN_char >
 #define TS_TEST_COMMAND 't'
